383aed3e4f553ef8a0a81d140629a4093b29d6c2
Adrian Del Balso <a.delbalso@gmail.com>
2015-01-04 17:06:28 -0500

    initial build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
+var browserify = require('gulp-browserify');
 
 gulp.task('default', function() {
   // place code for your default task here
@@ -16,3 +17,23 @@ gulp.task('serve', function() {
 
   gulp.watch(['*.html', 'styles/**/*.css', 'scripts/**/*.js'], {cwd: 'app'}, reload);
 });
+
+gulp.task('build',['js','css','html']);
+
+gulp.task('js',function(){
+  gulp.src('app/scripts/main.js')
+    .pipe(browserify({
+      debug: true
+    }))
+    .pipe(gulp.dest('./release/scripts'));
+});
+
+gulp.task('css',function(){
+  gulp.src('app/styles/main.css')
+    .pipe(gulp.dest('./release/styles'));
+});
+
+gulp.task('html',function(){
+  gulp.src('app/index.html')
+    .pipe(gulp.dest('./release'));
+})
